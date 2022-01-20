239490b5763628d745e7c9c4d69a8638e1b4b6d4
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-01-20 15:05:58 +0100

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* Die Satzbau-Maschine | Arrays */
+
+
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
+
+
