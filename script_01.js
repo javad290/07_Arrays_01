4c6664d0dacf21b9e95840941a6654e2808b27a7
javad290 <javad290@gmail.com>
2022-01-21 10:24:55 +0100

    01a fertig

--- script_01.js
+++ script_01.js
@@ -3,6 +3,47 @@
 /* Die Satzbau-Maschine | Arrays */
 
 
+//more of the same !!!!
+
+// **02.******Arrays ********* */
+
+let arr;
+
+// arr = new Array();
+
+// arr=[]
+// arr=[2,3,7,11]
+// output(arr)
+// output(arr.length)
+// output(arr[0])
+// output(arr[arr.length-1])
+
+
+
+
+
+
+
+
+// 01. Funktionalität mit Einzelparametern
+// Ich bin Javad Hajian
+output(getSentenceArr(["Ich", "bin", "Javad", "Hajian"]))
+function getSentenceArr(arr) {
+
+    const gap = " "
+    const punct = "."
+    const str = 
+        arr[0] + gap +
+        arr[1] + gap +
+        arr[2] + gap +
+        arr[3] +
+        punct;
+
+
+    return str
+}
+
+
 
 
 
